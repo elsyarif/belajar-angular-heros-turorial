2294f676d4db869b0132f78354b97ed1bd4a7b37
Syarif Hidayatulloh <id.syarif@gmail.com>
2022-06-19 22:33:59 +0700

    add service heros, message dependency

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../interface/hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,15 +11,22 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
+
   selectedHero?:Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
+   this.getheroes()
+  }
+
+  getheroes(): void{
+      this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
   }
 
   onSelect(hero: Hero): void{
-    this.selectedHero = hero
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: selected hero id=${hero.id}`);
   }
 }
