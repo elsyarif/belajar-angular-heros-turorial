28ea26ed79778b4137e7d9d8c1fc5682ff57afe2
Syarif Hidayatulloh <id.syarif@gmail.com>
2022-06-21 20:14:47 +0700

    add navigate, dashboard component and detail heros

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,18 +1,36 @@
+import { Location } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../hero.service';
 import { Hero } from '../interface/hero';
 
 @Component({
-  selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+   selector: 'app-hero-detail',
+   templateUrl: './hero-detail.component.html',
+   styleUrls: ['./hero-detail.component.css']
 })
 
 export class HeroDetailComponent implements OnInit {
 
-  @Input() hero?: Hero;
-  constructor() { }
+   hero: Hero | undefined;
+   constructor(
+      private route: ActivatedRoute,
+      private heroService: HeroService,
+      private location: Location
+   ) { }
 
-  ngOnInit(): void {
+   ngOnInit(): void {
+      this.gethero();
+   }
+
+   gethero(): void {
+      const id = Number(this.route.snapshot.paramMap.get('id'));
+
+      this.heroService.getHero(id)
+         .subscribe(hero => this.hero = hero)
   }
 
+  goBack(): void{
+   this.location.back()
+  }
 }
